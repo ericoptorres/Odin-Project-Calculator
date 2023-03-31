f682cbebd615d34c0449b026d219db0c4a97d76d
erico <erico_pt@hotmail.com>
2023-03-31 16:52:05 -0300

    operations functioning tho needs tweak

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const add = (a, b) => a + b
+const add = (a, b) => parseInt(a) + parseInt(b)
 const subtract = (a, b) => a - b
-const multiply = (a , b) => a * b
+const multiply = (a, b) => parseInt(a) * parseInt(b)  
 const divide = (a , b) => parseFloat((a / b).toFixed(2))
 
 let firstNumber
 let secondNumber
-let operation
+let operation = '0'
 
 const operate = (firstNum, secondNum, operation) => {
-    operation(firstNum, secondNum)
+    if (operation == '+'){
+        return add(firstNum, secondNum)
+    }
+    else if (operation == '-'){
+        return subtract(firstNum, secondNum)
+    }
+    else if (operation == '*'){
+        return multiply(firstNum, secondNum)
+    }
+    else if (operation == '/'){
+        return divide(firstNum, secondNum)
+    }
+    else return (alert('error'))
 }
+
+const display = document.querySelector(".display")
+let value = ''
+
+const numBtn = document.querySelectorAll('.num-btn')
+numBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent
+    })
+})
+
+const operationBtn = document.querySelectorAll(".operation-btn")
+operationBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        firstNumber = display.textContent
+        operation = button.textContent
+        display.textContent += ` ${operation} `
+    })
+})
+
+const enter = document.querySelector('.enter')
+enter.addEventListener('click', () => {
+    secondNumber = display.textContent.split(' ')[2]
+    display.textContent = operate(firstNumber, secondNumber, operation)
+})
+
